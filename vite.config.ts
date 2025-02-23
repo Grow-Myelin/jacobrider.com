f140040a779b9008b3f10c33d8f6cbcf61f1e598
Grow-Myelin <jacob.rider34@gmail.com>
2025-02-23 15:57:04 -0500

    Changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: 'jacobrider.com',
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.JPG', '**/*.png', '**/*.jpg', '**/*.jpeg'],
 })
